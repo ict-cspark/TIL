6cb894bd7e5845f1304b4122a0e859e72836aa71
ict-cspark <ict.cspark@gmail.com>
2022-02-10 22:01:23 +0900

    solve

---
+++ Algorithm/Baekjoon/Step9/09_Math02_3009.py
@@ -0,0 +1,13 @@
+# Baekjoon Algorithm 9단계 Math02 - 네 번째 점
+
+'''
+세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
+'''
+
+result = 0
+for i in range(3):
+    x,y = map(int,input().split())
+    temp_x = 0
+    temp_x = 0
+
+print(result)
